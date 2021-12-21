6f0b4501ed0eef8f4df073aba03c16492e874298
Daniel Calvin <arockne2003@yahoo.com>
2021-12-21 13:16:37 -0700

    Created index and show actions

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,18 @@
 class GroupsController < ApplicationController
+
+  def index
+    groups = Group.all
+    render json: groups, status: :ok
+  end
+
+  def show
+    group = Group.find(group_params[:id])
+    render json: group, include: :events
+  end
+
+  private
+
+    def group_params
+      params.permit
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+dakota = User.create(username: 'dakota', email: 'dakota@flatironschool.com')
+
+sam = User.create(username: 'sam', email: 'sam.boahen@flatironschool.com')
+
+group = Group.create(name: 'Online software', location: 'Online')
+
+group.members = [dakota, sam]
+
+lecture_1 = group.events.create(
+  user: dakota,
+  title: 'Rails Fundamentals',
+  start_time: 1.hour.ago,
+  end_time: 1.hour.from_now
+)
+
+lecture_1.attendees = [dakota, sam]
