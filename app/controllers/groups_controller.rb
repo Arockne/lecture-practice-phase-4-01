d4b6b3cb7fb23f466b5242db122166ceda8a836f
Daniel Calvin <arockne2003@yahoo.com>
2021-12-22 15:20:17 -0700

    Implement create action and strong params

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-
+rescue_from ActiveRecord::InvalidRecord, with: :render_unproccesable_entity
   def index
     groups = Group.all
     render json: groups, status: :ok
@@ -8,5 +8,16 @@ class GroupsController < ApplicationController
   def show
     group = Group.find(params[:id])
     render json: group, include: :events
+  end
+
+  def create
+    group = Group.create!(group_params)
+    render json: group, status: :created
+  end
+
+  private
+
+  def group_params
+    params.permit(:name, :location)
   end
 end
