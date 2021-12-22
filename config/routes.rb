54f3831dc2972ff976d9af5d63c53a193ea8f290
Daniel Calvin <arockne2003@yahoo.com>
2021-12-22 15:21:34 -0700

    Allow update route for groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :events, only: [:index, :show]
-  resources :groups, onnly: [:index, :show]
+  resources :groups, onnly: [:index, :show, :create, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
