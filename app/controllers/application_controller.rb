5b0f5f7b40a658f8be15ef2eada645516be3aeef
Daniel Calvin <arockne2003@yahoo.com>
2021-12-22 15:22:09 -0700

    Implement error hanndling for invalid record and a temp current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+rescue_from ActiveRecord::InvalidRecord, with: :render_unproccesable_entity
+
+
+  private
+
+  def current_user
+    User.first
+  end
+
+  def render_unproccesable_entity(invalid)
+    render json: invalid.record.full_messages, status: :unprocessable_entity
+  end
 end
