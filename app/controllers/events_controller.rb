f53b94f4345d269b2066c047bf59e12f5d941fe6
Daniel Calvin <arockne2003@yahoo.com>
2021-12-22 15:55:46 -0700

    Implement index, show, create actions and strong params

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,23 @@
 class EventsController < ApplicationController
+
+  def index
+    events = Event.all
+    render json: events, status: :ok
+  end
+
+  def show
+    event = Event.find(params[:id])
+    render json: event, include: :attendees, status: :ok
+  end
+
+  def create
+    event = current_user.events.create!(event_params)
+    render json: event, status: :created
+  end
+
+  private
+
+  def event_params
+    params.permit(:title, :description, :location, :start_time, :end_time, :group_id)
+  end
 end
