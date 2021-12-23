d71150304292698281859b424bbdcac0a6c1103b
Daniel Calvin <arockne2003@yahoo.com>
2021-12-22 18:26:43 -0700

    Add post route to user_groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :events, only: [:index, :show]
   resources :groups, onnly: [:index, :show, :create, :update]
+  resources :user_groups, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
