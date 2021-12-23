5496e121b8241e852b5202671b8677903e0b99a8
Daniel Calvin <arockne2003@yahoo.com>
2021-12-23 15:38:52 -0700

    Add routes for updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :events, only: [:index, :show, :destroy]
+  resources :events
   resources :groups, onnly: [:index, :show, :create, :update]
-  resources :user_groups, only: [:create]
-  resources :user_events, only: [:create]
+  resources :user_groups, only: [:create, :destroy]
+  resources :user_events, only: [:create, :destroy, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
