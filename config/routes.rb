3e781176e3301751d552af0268ac3a2fda4b4104
Daniel Calvin <arockne2003@yahoo.com>
2021-12-23 11:26:22 -0700

    Add route to user_events

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :events, only: [:index, :show]
   resources :groups, onnly: [:index, :show, :create, :update]
   resources :user_groups, only: [:create]
+  resources :user_events, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
