1010bd096077f3fe7ef849bad80b37d4adb3c8c8
Daniel Calvin <arockne2003@yahoo.com>
2021-12-22 18:25:26 -0700

    Implement create action with strong params

--- app/controllers/user_groups_controller.rb
+++ app/controllers/user_groups_controller.rb
@@ -1,2 +1,13 @@
 class UserGroupsController < ApplicationController
+
+  def create
+    user_group = current_user.user_groups.create!(user_group_params)
+    render json: user_group, status: :created
+  end
+
+  private
+
+  def user_group_params
+    params.permit(:group_id)
+  end
 end
