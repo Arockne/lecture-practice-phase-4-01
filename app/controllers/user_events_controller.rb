b374250e7a13d2785a8df8e602655368ed61b762
Daniel Calvin <arockne2003@yahoo.com>
2021-12-23 11:32:42 -0700

    Refactor create action to utilize exception handling

--- app/controllers/user_events_controller.rb
+++ app/controllers/user_events_controller.rb
@@ -1,11 +1,7 @@
 class UserEventsController < ApplicationController
   def create
-    user_event = current_user.user_events.new(user_event_params)
-    if user_event.save
-      render json: user_event, status: :created
-    else
-      render json: user_event.errors, status: :unprocessable_entity
-    end
+    user_event = current_user.user_events.create!(user_event_params)
+    render json: user_event, status: :created
   end
 
   private
