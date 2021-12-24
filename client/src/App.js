154d9f7082abab7b13b1de10c9b710216db5bd99
Daniel Calvin <arockne2003@yahoo.com>
2021-12-24 14:22:18 -0700

    Add lecture code to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import GroupsContainer from './components/GroupsContainer'
+import { Switch, Route, NavLink, BrowserRouter as Router } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavLink to="/groups">Groups</NavLink>
+        <Switch>
+          <Route path="/groups">
+            <GroupsContainer />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
