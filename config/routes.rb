2261e294fb811d9e0f0edce0683040b1bfbb5b49
Daniel Calvin <arockne2003@yahoo.com>
2021-12-24 11:38:52 -0700

    Add index routes to both user_groups and user_events

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :events
   resources :groups, onnly: [:index, :show, :create, :update]
-  resources :user_groups, only: [:create, :destroy]
-  resources :user_events, only: [:create, :destroy, :update]
+  resources :user_groups, only: [:index, :create, :destroy]
+  resources :user_events, only: [:index, :create, :destroy, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
