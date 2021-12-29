345f8e2abf064aef3de0654b51b1554237b2eb25
Daniel Calvin <arockne2003@yahoo.com>
2021-12-29 15:09:15 -0700

    Implement login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
 
   get '/me', to: 'users#show'
   post '/signup', to: 'users#create'
+
+  delete '/logout', to: 'sessions#destroy'
+  post '/login', to: 'sessions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
