6608781780f99c4b6452a3aa6c999b117fe21d9f
Daniel Calvin <arockne2003@yahoo.com>
2021-12-29 11:29:17 -0700

    Implement show and create actions with strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+  def show
+    if current_user
+      render json: current_user
+    else
+      render json: {}, status: :unauthorized
+    end
+  end
+
+  def create
+    user = User.create!(user_params)
+    session[:user_id] = user.id
+    render json: user, status: :created
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password, :password_confirmation)
+  end
 end
