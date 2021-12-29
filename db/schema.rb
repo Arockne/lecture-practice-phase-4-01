888b1eccff4f16dd48a4ea18572cd519c5fc114d
Daniel Calvin <arockne2003@yahoo.com>
2021-12-29 10:43:08 -0700

    Migrate password_digest collumn to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_23_220103) do
+ActiveRecord::Schema.define(version: 2021_12_29_172956) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2021_12_23_220103) do
     t.text "bio"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "events", "groups"
