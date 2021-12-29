e025412844fece17f0381380a86f880f430fbd3c
Daniel Calvin <arockne2003@yahoo.com>
2021-12-29 11:27:50 -0700

    Implement current user to assign current_user instance variable only when the current_user is nil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ rescue_from ActiveRecord::RecordInvalid, with: :render_unproccesable_entity
   private
 
   def current_user
-    User.first
+   @current_user ||= User.find(session[:user_id])
   end
 
   def render_unproccesable_entity(invalid)
